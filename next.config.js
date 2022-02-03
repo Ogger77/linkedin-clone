0fdb66434f07de45749a3eb5cb2f075bf5cb0a8e
Dinh Vu <vud@kean.edu>
2022-02-03 11:28:53 -0500

    initialize

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "logos-marques.com",
+      "rb.gy",
+      "media.licdn.com",
+      "static.adweek.com",
+      "cdn.onlinewebfonts.com",
+      "1000logos.net",
+      "lofrev.net",
+      "wallpapercave.com",
+      "s3.amazonaws.com",
+      "business.linkedin.com",
+    ],
+  },
+};
